6501b9299a8048329a03a63feca90223396153c0
crazydemon0803 <79695211+crazydemon0803@users.noreply.github.com>
2021-11-24 15:57:28 +0530

    Update Election.sol

--- Election.sol
+++ Election.sol
@@ -1,5 +1,4 @@
 pragma solidity 0.4.24;
-
 contract Election {
     struct Candidate {
         uint id;
@@ -10,11 +9,9 @@ contract Election {
     mapping(address => bool) public voters;
     mapping(uint => Candidate) public candidates;
     uint public candidatesCount;
-
     event votedEvent (
         uint indexed _candidateId
     );
-
     constructor () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
@@ -27,27 +24,19 @@ contract Election {
         addCandidate("Candidate 9");
         addCandidate("Candidate 10");
     }
-
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
-
     function end () public {
         goingon = false;
     }
-
     function vote (uint _candidateId) public {
         require(!voters[msg.sender],"Already voted");
-
         require(_candidateId > 0 && _candidateId <= candidatesCount,"Invalid candidate");
-
         require(goingon,"Election ended");
-
         voters[msg.sender] = true;
-
         candidates[_candidateId].voteCount ++;
-
         emit votedEvent(_candidateId);
     }
 }
